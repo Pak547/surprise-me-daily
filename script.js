14a806a46db7cd5a90539799f25d02a95c9097e9
Pak <pakapongm@gmail.com>
2023-10-31 14:03:58 -0700

    Touched up on weather API included cityName input and key... still 400 error. tixAPI currently trying to implement into HTML

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const apiKey = "bad2585150121c9b32104915c6e8ce3f"
-
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}"
-
+const weatherKey = "bad2585150121c9b32104915c6e8ce3f"
+let cityName = "";
+const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&units=imperial&appid=" + weatherKey
 async function getWeather() {
 	try {
-		let response = await fetch(apiUrl)
+		let response = await fetch(weatherUrl)
 		console.log(response);
 		if (!response.ok) throw new Error("not a valid response");
 		let data = await response.json();
@@ -16,18 +15,24 @@ async function getWeather() {
 
 
 // Tasnim code above here
-
-const APIkey = "wmaoc2ZzZXf8620JjoaSV5OEFlvJNJ84"
-const url = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey="+ APIkey
+const content = document.querySelector(".showEvents");
+const tixKey = "wmaoc2ZzZXf8620JjoaSV5OEFlvJNJ84"
+const tixUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey="+ tixKey
 
 async function getData() {
   try {
-    let response = await fetch(url);
+    let response = await fetch(tixUrl);
     console.log(response);
+
+    response.events.textContent(content);
     if (!response.ok) throw new Error('not a valid response');
-    let dataobj = await response.json();
-    console.log(dataobj);
+    let data = await response.json();
+    console.log(data);
   } catch (error) {
     console.warn(error.message);
   }
 }
+
+
+
+
