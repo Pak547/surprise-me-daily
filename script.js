b6f3ed296609186ad93a37f7b7e01c750820ebe4
Pak <pakapongm@gmail.com>
2023-10-31 20:26:38 -0700

    Updated the HTML both sections to include tailwind responsiveness, also included an empty div for both API insertion. In javascript currently working on a function to inject

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const weatherKey = "bad2585150121c9b32104915c6e8ce3f"
 let cityName = "";
 const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&units=imperial&appid=" + weatherKey
@@ -12,27 +13,26 @@ async function getWeather() {
 		console.warn(error.message);
 	}
 }
-
+*/
 
 // Tasnim code above here
-const content = document.querySelector(".showEvents");
 const tixKey = "wmaoc2ZzZXf8620JjoaSV5OEFlvJNJ84"
 const tixUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey="+ tixKey
 
+// function to retrieve data
 async function getData() {
   try {
-    let response = await fetch(tixUrl);
+    const response = await fetch(tixUrl);
     console.log(response);
-
-    response.events.textContent(content);
-    if (!response.ok) throw new Error('not a valid response');
-    let data = await response.json();
-    console.log(data);
+    // if theres an error then say not valid and show status
+    if (!response.ok) throw new Error('not a valid response' + response.status);
+    const data = await response.json();
+    // variable events to grab the event param inside fetch
+    const events = data._embedded.events
+    console.log(data._embedded.events);
+// catching the error
   } catch (error) {
     console.warn(error.message);
   }
 }
-
-
-
-
+// function to inject into html
