44db4388f1632638a687aa12aa767da42ebb0c42
Pak <pakapongm@gmail.com>
2023-11-02 02:43:25 -0700

    Previous and next buttons work and a limiter, Propagated data into HTML and shows an image. Next work on creating an anchor tag to link URL for user to buy tickets

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var temperature = document.querySelector(".temperature");
 var summary = document.querySelector(".summary");
 var loc = document.querySelector(".location");
 
-var cityName = "London";
+var cityName = "Long Beach";
 const weatherKey = "bad2585150121c9b32104915c6e8ce3f"; // not best practice
 const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + weatherKey;
 
@@ -21,32 +21,43 @@ fetch(weatherUrl)
   })
   .then(handleWeatherResponse);
 
-// psuedo code
-// Tasnim code above here
-
-
-let tixCity = "Riverside";
-const tixKey = "wmaoc2ZzZXf8620JjoaSV5OEFlvJNJ84"
-const tixUrl = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + tixCity + "&apikey=" + tixKey
-// empty string populate into userInput 
-var eventIncr = "0";
-var leftArrowBtn = document.querySelector("#left-arrow");
-var rightArrowBtn = document.querySelector("#right-arrow")
-// onclick ++ .then
-// JSON.stringify to allow it to go back into the array button increment and decrement
-// each time they click the button it changes the event
-
-const leftIncr = function minusMinus(eventIncr) 
-{
-  if(eventIncr === String){
-    JSON.parse(eventIncr);
-  } else {eventIncr--}
-  return(eventIncr);
+  // Tasnim code above here
+  
+  let tixCity = "Long Beach";
+  const tixKey = "wmaoc2ZzZXf8620JjoaSV5OEFlvJNJ84" // not best practice
+  const tixUrl = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + tixCity + "&apikey=" + tixKey
+  
+  // empty array because the api array to then reassign on line 105
+  let eventList = [];
+  let eventIncr = 0;
+  const leftArrowBtn = document.querySelector("#left-arrow");
+  const rightArrowBtn = document.querySelector("#right-arrow")
+  
+  const leftIncr = function minusMinus() {
+    // creating limit
+    if(eventIncr > 0){
+      eventIncr--;
+      console.log(eventIncr);
+      showData();
+    } else {
+      eventIncr = 0
+      console.log(eventIncr);
+    }
 }
-
 leftArrowBtn.addEventListener('click', leftIncr)
 
-const rightIncr = function plusPlus(eventIncr) {
+const rightIncr = function plusPlus() {
+  // creating a limit
+  if ( eventIncr < 19){
+  eventIncr++;
+  console.log(eventIncr);
+  showData();
+  } else {
+    eventIncr = 19
+    console.log(eventIncr);
+    // cursor change? or modal to tell user stop
+  }
+  /*
   if(eventIncr === String){
     JSON.parse(eventIncr);
   } else {eventIncr++}
@@ -55,48 +66,25 @@ const rightIncr = function plusPlus(eventIncr) {
     // convert to String
   }
   console.log(eventIncr)
-  return(typeof eventIncr);
+  return(eventIncr);
+  */
 }
-
 rightArrowBtn.addEventListener('click', rightIncr)
 
-// if string is true then convert to integer 
-  // else if integer is true then proceed
-  // JSON parse after button click to allow it to be changed again
-  // if event anchor is < 0 return 0
-  // if event incre is > 20 return 20
-// function to retrieve data
-async function getData() {
-  try {
-    const response = await fetch(tixUrl);
-    // if theres an error then say not valid and show status
-    if (response.ok) {
-      let data = await response.json();
-      // variable events to grab the event param inside fetch
-      // bracket notation with Richard's help to call data
-      let eachEvent = data["_embedded"]["events"];
-      let eventData = data["_embedded"]["events"][eventIncr]["name"];
-      let dateData = data["_embedded"]["events"][eventIncr]["dates"]["start"]["localDate"];
-      let timeData = data["_embedded"]["events"][eventIncr]["dates"]["start"]["localTime"];
-      let picData = data["_embedded"]["events"][eventIncr]["images"]["0"]
-
-      console.log(eachEvent);
-      console.log(eventData);
-      console.log(dateData);
-      console.log(timeData);
+function showData () {
+      const eventData = eventList[eventIncr].name
+      const dateData = eventList[eventIncr].dates.start.localDate
+      const timeData = eventList[eventIncr].dates.start.localTime
+      const picData = eventList[eventIncr].images[0].url
       console.log(picData);
-
       const eventContainer = document.getElementById('showEvents');
-
-      // based on the api array we might have to create a for loop for each event, name, date, 0-6
-      // cant access values using only events because each page is a integer for example data._embedded.events.0.names
-      // so we need to identify the integer in a for loop to be able to use it... index of I failed will try more options
-
-      // loop to plug 6 events into the HTML
-
-      for (let i = 0; i < 1; i++) {
+      // reset the propagation 
+      eventContainer.innerHTML = "";
         const eventElement = document.createElement('div');
         eventElement.className = 'event';
+        // shows picture, note .src
+        const eventPic = document.createElement('img');
+        eventPic.src = picData
         // shows title
         const eventTitle = document.createElement('h2');
         eventTitle.textContent = eventData
@@ -106,16 +94,26 @@ async function getData() {
         // shows time in military
         const eventTime = document.createElement('p');
         eventTime.textContent = timeData
-
+        // append everything into the div as a child
+        eventElement.appendChild(eventPic);
         eventElement.appendChild(eventTitle);
         eventElement.appendChild(eventDate);
         eventElement.appendChild(eventTime);
-
+        // finally we propagate into the container
         eventContainer.appendChild(eventElement);
-      }
+}
+
+async function getData() {
+  try {
+    const response = await fetch(tixUrl);
+    // if theres an error then say not valid and show status
+    if (response.ok) {
+      const data = await response.json();
+      eventList = data._embedded.events;
+      showData();
     }
-    // catching the error
   } catch (error) {
     console.warn(error.message);
   }
-}
+} 
+getData();
