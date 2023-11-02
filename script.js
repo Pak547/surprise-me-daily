38dd5f177b56e4de695db8cd77fbc8febc84802d
Tasneem <thalim.glam@gmail.com>
2023-11-01 18:56:29 -0700

    Reorganizing the consistency of the code

--- script.js
+++ script.js
@@ -1,50 +1,23 @@
-
-var lon;
-var lat;
+//Weather API
 var temperature = document.querySelector(".temperature");
 var summary = document.querySelector(".summary");
 var loc = document.querySelector(".location");
 
-window.addEventListener("load", () => {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition((position) => {
-			lon = position.coords.longitude;
-			lat = position.coords.latitude;
-
-			const api = "bad2585150121c9b32104915c6e8ce3f";
-
-			const base =
-				`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` + `lon=${lon}&appid=bad2585150121c9b32104915c6e8ce3f`;
-
-			fetch(base)
-				.then((response) => {
-					return response.json();
-				})
-				.then((data) => {
-					console.log(data);
-					temperature.textContent = Math.floor(data.main.temp) + "°F";
-					summary.textContent = data.weather[0].description;
-					loc.textContent = data.name + "," + data.sys.country;
-					document.querySelector("#current-weather").setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
-				});
-		});
-	}
-});
-
 const weatherKey = "bad2585150121c9b32104915c6e8ce3f"
-let cityName = "";
-const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&units=imperial&appid=" + weatherKey
-async function getWeather() {
-	try {
-		let response = await fetch(weatherUrl)
-		console.log(response);
-		if (!response.ok) throw new Error("not a valid response");
-		let data = await response.json();
-		console.log(data);
-	} catch (error) {
-		console.warn(error.message);
-	}
-}
+var cityName = "London";
+const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + weatherKey
+
+fetch(weatherUrl)
+  .then((response){
+    return response.json();
+  })
+  .then((data){
+    console.log(data);
+    temperature.textContent = Math.floor(data.main.temp) + "°F";
+    summary.textContent = data.weather[0].description;
+    loc.textContent = data.name + "," + data.sys.country;
+    document.querySelector("#current-weather").setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
+  });
 
 
 // Tasnim code above here
@@ -52,7 +25,7 @@ async function getWeather() {
 
 let tixCity = "Riverside";
 const tixKey = "wmaoc2ZzZXf8620JjoaSV5OEFlvJNJ84"
-const tixUrl = "https://app.ticketmaster.com/discovery/v2/events.json?city="+tixCity+"&apikey="+ tixKey
+const tixUrl = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + tixCity + "&apikey=" + tixKey
 // empty string populate into userInput 
 var eventIncr = 0;
 // onclick ++ .then
@@ -64,58 +37,58 @@ async function getData() {
   try {
     const response = await fetch(tixUrl);
     // if theres an error then say not valid and show status
-    if (response.ok){
-    let data = await response.json();
-    // variable events to grab the event param inside fetch
-    // bracket notation with Richard's help to call data
-    let eachEvent = data["_embedded"]["events"];
-    let eventData = data["_embedded"]["events"][eventIncr]["name"];
-    //var name1 = data["_embedded"]["events"]["1"]["name"];
-    //var name2 = data["_embedded"]["events"]["2"]["name"];
-    let dateData = data["_embedded"]["events"]["0"]["dates"]["start"]["localDate"];
-   // var date1 = data["_embedded"]["events"]["1"]["dates"]["start"]["localDate"];
-   // var date2 = data["_embedded"]["events"]["2"]["dates"]["start"]["localDate"];
-    let timeData = data["_embedded"]["events"]["0"]["dates"]["start"]["localTime"];
-   // var time1 = data["_embedded"]["events"]["1"]["dates"]["start"]["localTime"];
-   // var time2 = data["_embedded"]["events"]["2"]["dates"]["start"]["localTime"];
-   let picData = data["_embedded"]["events"]["0"]["images"]["0"]
+    if (response.ok) {
+      let data = await response.json();
+      // variable events to grab the event param inside fetch
+      // bracket notation with Richard's help to call data
+      let eachEvent = data["_embedded"]["events"];
+      let eventData = data["_embedded"]["events"][eventIncr]["name"];
+      //var name1 = data["_embedded"]["events"]["1"]["name"];
+      //var name2 = data["_embedded"]["events"]["2"]["name"];
+      let dateData = data["_embedded"]["events"]["0"]["dates"]["start"]["localDate"];
+      // var date1 = data["_embedded"]["events"]["1"]["dates"]["start"]["localDate"];
+      // var date2 = data["_embedded"]["events"]["2"]["dates"]["start"]["localDate"];
+      let timeData = data["_embedded"]["events"]["0"]["dates"]["start"]["localTime"];
+      // var time1 = data["_embedded"]["events"]["1"]["dates"]["start"]["localTime"];
+      // var time2 = data["_embedded"]["events"]["2"]["dates"]["start"]["localTime"];
+      let picData = data["_embedded"]["events"]["0"]["images"]["0"]
 
-    console.log(eachEvent);
-    console.log(eventData);
-    console.log(dateData);
-    console.log(timeData);
-    console.log(picData); 
+      console.log(eachEvent);
+      console.log(eventData);
+      console.log(dateData);
+      console.log(timeData);
+      console.log(picData);
 
-    const eventContainer = document.getElementById('showEvents');
+      const eventContainer = document.getElementById('showEvents');
 
-// based on the api array we might have to create a for loop for each event, name, date, 0-6
-// cant access values using only events because each page is a integer for example data._embedded.events.0.names
-// so we need to identify the integer in a for loop to be able to use it... index of I failed will try more options
+      // based on the api array we might have to create a for loop for each event, name, date, 0-6
+      // cant access values using only events because each page is a integer for example data._embedded.events.0.names
+      // so we need to identify the integer in a for loop to be able to use it... index of I failed will try more options
 
-// loop to plug 6 events into the HTML
+      // loop to plug 6 events into the HTML
 
-    for (let i=0; i<1; i++){
-      const eventElement = document.createElement('div');
-     eventElement.className = 'event';
-      // shows title
-     const eventTitle = document.createElement('h2');
-     eventTitle.textContent = eventData
-      //shows date in YYYY-MM-DD
-     const eventDate = document.createElement('p');
-     eventDate.textContent = dateData
-     // shows time in military
-     const eventTime = document.createElement('p');
-     eventTime.textContent = timeData
+      for (let i = 0; i < 1; i++) {
+        const eventElement = document.createElement('div');
+        eventElement.className = 'event';
+        // shows title
+        const eventTitle = document.createElement('h2');
+        eventTitle.textContent = eventData
+        //shows date in YYYY-MM-DD
+        const eventDate = document.createElement('p');
+        eventDate.textContent = dateData
+        // shows time in military
+        const eventTime = document.createElement('p');
+        eventTime.textContent = timeData
 
-     eventElement.appendChild(eventTitle);
-     eventElement.appendChild(eventDate);
-     eventElement.appendChild(eventTime);
-     
-     eventContainer.appendChild(eventElement);
+        eventElement.appendChild(eventTitle);
+        eventElement.appendChild(eventDate);
+        eventElement.appendChild(eventTime);
 
+        eventContainer.appendChild(eventElement);
+
+      }
     }
-    }
-// catching the error
+    // catching the error
   } catch (error) {
     console.warn(error.message);
   }
@@ -125,11 +98,11 @@ addEventListener.onclick("#left-arrow"); {
   JSON.stringify(eventIncr)
 }
 addEventListener.onclick("#right-arrow"); {
-eventIncr++;
-JSON.stringify(eventIncr)
-// if string is true then convert to integer 
-// else if integer is true then proceed
-// JSON parse after button click to allow it to be changed again
-// if event anchor is < 0 return 0
-// if event incre is > 20 return 20
+  eventIncr++;
+  JSON.stringify(eventIncr)
+  // if string is true then convert to integer 
+  // else if integer is true then proceed
+  // JSON parse after button click to allow it to be changed again
+  // if event anchor is < 0 return 0
+  // if event incre is > 20 return 20
 }
