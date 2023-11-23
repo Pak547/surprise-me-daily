a889469f256a1606444a0dca0432b7c1a9511e5e
Pak <pakapongm@gmail.com>
2023-11-22 17:27:33 -0800

    took out comments and adjusted var to es6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,13 @@
-//Weather API
-var searchInput = document.querySelector(".search")
-var searchButton = document.querySelector(".start-btn")
-var temperature = document.querySelector(".temperature");
-var summary = document.querySelector(".summary");
-var loc = document.querySelector(".location");
+const searchInput = document.querySelector(".search")
+const searchButton = document.querySelector(".start-btn")
+const temperature = document.querySelector(".temperature");
+const summary = document.querySelector(".summary");
+const loc = document.querySelector(".location");
 
-var cityName = "";
-const weatherKey = "bad2585150121c9b32104915c6e8ce3f"; // not best practice
+let cityName = "";
+const weatherKey = "bad2585150121c9b32104915c6e8ce3f"; 
 
 function handleWeatherResponse(data) {
-  console.log("grabbing new weather resp data");
   temperature.textContent = "Temperature: " + Math.floor(data.main.temp) + "°F";
   summary.textContent = "Weather Condition " + data.weather[0].description;
   loc.textContent = data.name + ", " + data.sys.country;
@@ -17,15 +15,12 @@ function handleWeatherResponse(data) {
 }
 
 function searchWeather(cityName) {
-  console.log("searching weather")
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${weatherKey}`)
   .then(function (response) {
     return response.json();
   })
   .then(handleWeatherResponse);
 }
-// Tasnim code above here
-// Below is ticketmaster 
 
 const TIX_KEY = "wmaoc2ZzZXf8620JjoaSV5OEFlvJNJ84" // not best practice
 const TIX_BASE_PATH = `https://app.ticketmaster.com/discovery/v2/events.json`;
@@ -41,16 +36,13 @@ searchButton.addEventListener("click", function (event) {
   localStorage.setItem("Saved Events", JSON.stringify(eventList));
 }
 )
-// empty array because the api array to then reassign on line 105
-// this array is causing the search input to not work properly because the list is going into the array
+
 let eventList = [];
-// console.log("empty array " + eventList)
 let eventIncr = 0;
 const leftArrowBtn = document.querySelector("#left-arrow");
 const rightArrowBtn = document.querySelector("#right-arrow");
 
 const leftIncr = function minusMinus() {
-  // creating limit
   if (eventIncr > 0) {
     eventIncr--;
     console.log(eventIncr);
@@ -71,19 +63,8 @@ const rightIncr = function plusPlus() {
   } else {
     eventIncr = 19
     console.log(eventIncr);
-    // cursor change? or modal to tell user stop
   }
-  /*
-  if(eventIncr === String){
-    JSON.parse(eventIncr);
-  } else {eventIncr++}
-  while (eventIncr === Number){
-    // While this statement is true... 
-    // convert to String
-  }
-  console.log(eventIncr)
-  return(eventIncr);
-  */
+
 }
 rightArrowBtn.addEventListener('click', rightIncr)
 
@@ -105,46 +86,32 @@ async function getData() {
 
 function showData(eventList) {
   console.log("event list", eventList)
-  // title
   const eventData = eventList[eventIncr].name
-  //date
   const dateData = eventList[eventIncr].dates.start.localDate
-  //time
   const timeData = eventList[eventIncr].dates.start.localTime
-  // img jpg
   const picData = eventList[eventIncr].images[0].url
-  // buy ticket link
   const linkData = eventList[eventIncr].url
-  // beginning of html inject
   const eventContainer = document.getElementById('showEvents');
-  // reset the propagation 
   eventContainer.innerHTML = "";
   const eventElement = document.createElement('div');
   eventElement.className = 'event';
-  // shows picture, note .src
   const eventPic = document.createElement('img');
   eventPic.src = picData
   eventPic.className = 'innerImg'
-  // shows title
   const eventTitle = document.createElement('h2');
   eventTitle.textContent = eventData
-  // buy ticket link
   const eventLink = document.createElement('a');
   eventLink.href = linkData
   eventLink.className = 'innerAnchor'
   eventLink.textContent = eventLink.href
-  //shows date in YYYY-MM-DD
   const eventDate = document.createElement('p');
   eventDate.textContent = dateData
-  // shows time in military
   const eventTime = document.createElement('p');
   eventTime.textContent = timeData
-  // append everything into the div as a child
   eventElement.appendChild(eventPic);
   eventElement.appendChild(eventTitle);
   eventElement.appendChild(eventLink);
   eventElement.appendChild(eventDate);
   eventElement.appendChild(eventTime);
-  // finally we propagate into the container
   eventContainer.appendChild(eventElement);
 }
